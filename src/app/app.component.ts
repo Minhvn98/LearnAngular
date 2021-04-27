be3d66067efd1596fe336f137b859b3400a32444
Minhvn98 <Minhvn62@wru.vn>
2021-04-27 21:09:04 +0700

    Attribute derective explore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'LearnAngular';
+  title = 'Explore Attribute Derective!';
+
+  isRed = false;
+  isGreen = false;
 }
